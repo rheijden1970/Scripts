96ce0ccfc692fbe1dc6c2a39393299718617ecbb
rheijden1970 <wtpooh1970@gmail.com>
2018-12-26 07:21:03 +0100

    Update nfs2fstab.sh

--- nfs2fstab.sh
+++ nfs2fstab.sh
@@ -1,9 +1,13 @@
-sharefolder=
-sharedfolder=
-user=
-foldername=
+#!/bin/bash
 
-mkdir /home/$user/$foldername
+#datalines
+ip=1.1.1.1
+sharefolder="/mnt/lokaal/sdc/sabnzbd/downloads/complete/foldername"
+user=richard
+foldername=test
 
-sudo cp /etc/fstab /etc/fstab.backup
-echo "1.1.1.1:/mnt/$sharefolder/$sharedfolder /home/$user/$foldername nfs noauto,x-systemd.automount,x-systemd.device-timeout=10,timeo=14,x-systemd.idle-timeout=1min 0 0" | sudo tee -a /etc/fstab >/dev/null
+#make the folder for the nfs share
+mkdir -p /home/$user/$foldername
+
+#sudo cp /etc/fstab /etc/fstab.backup
+echo "$ip:$sharefolder /home/$user/$foldername nfs noauto,x-systemd.automount,x-systemd.device-timeout=10,timeo=14,x-systemd.idle-timeout=1min 0 0" | sudo tee -a /home/$user/fstab >/dev/null
