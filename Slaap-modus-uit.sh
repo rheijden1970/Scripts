85e0db4ec7bcef31dc52b793e1bb257e4d64119c
rheijden1970 <wtpooh1970@gmail.com>
2019-01-29 21:45:06 +0100

    Update Slaap-modus-uit.sh

--- Slaap-modus-uit.sh
+++ Slaap-modus-uit.sh
@@ -1,2 +1,4 @@
 #Zet slaap mode uit in linux
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+xfconf-query -c xfce4-session -np '/shutdown/ShowHibernate' -t 'bool' -s 'false' 
+xfconf-query -c xfce4-session -np '/shutdown/ShowSuspend' -t 'bool' -s 'false'
