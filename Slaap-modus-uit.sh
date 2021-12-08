79980c2c0422c8e0cf429b507a44bb94da469012
rheijden1970 <24871290+rheijden1970@users.noreply.github.com>
2021-12-08 18:21:14 +0100

    Update Slaap-modus-uit.sh

--- Slaap-modus-uit.sh
+++ Slaap-modus-uit.sh
@@ -1,4 +1,4 @@
-#Zet slaap mode uit in linux
+#Zet slaap mode uit in archlinux xfce
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 xfconf-query -c xfce4-session -np '/shutdown/ShowHibernate' -t 'bool' -s 'false' 
 xfconf-query -c xfce4-session -np '/shutdown/ShowSuspend' -t 'bool' -s 'false'
